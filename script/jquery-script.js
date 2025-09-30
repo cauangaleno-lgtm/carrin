e1206ff994971aa328e809ae0a94eb291275a7ff
cauangaleno-lgtm <cauan.galeno@aluno.senai.br>
2025-09-30 09:31:36 -0300

    Add files via upload

---
+++ script/jquery-script.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    $("#botao-cadastrar").click(function(){
+        $("#form-cadastrar").slideToggle("slow")
+        $("#sec-login").slideToggle("slow")
+        $("#botao-cadastrar").hide()
+    })
+   
+})
